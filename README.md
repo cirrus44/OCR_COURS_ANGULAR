2d0b27b4671aeed1d1f2677d06f47b8b30c0f418
orlan.martin <martin.orlan@gmail.com>
2019-09-27 13:08:01 +0200

    exo1 config auteur et email

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BLOGOCR
+# BLOG
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.4.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'BLOG-OCR';
+  title = 'Posts';
+
+  posts = [
+    {
+      _title: 'Mon premier post',
+      _content: 'Utilité de connaître le secret de fabrication. Résistance est le mot profond d un sacrilège et que cette part est réduite au minimum possible par la charité, au nom vengeur de la conscience.',
+      loveIts: 0,
+      created_at: new Date(),
+      point:0
+    },
+    {
+      _title: 'Mon deuxième post',
+      _content: 'Utilité de connaître le secret de fabrication. Résistance est le mot profond d un sacrilège et que cette part est réduite au minimum possible par la charité, au nom vengeur de la conscience.',
+      loveIts: 0,
+      created_at: new Date(),
+      point:0
+    },
+    {
+      _title: 'Encore un post',
+      _content: 'Utilité de connaître le secret de fabrication. Résistance est le mot profond d un sacrilège et que cette part est réduite au minimum possible par la charité, au nom vengeur de la conscience.',
+      loveIts: 0,
+      created_at: new Date(),
+      point:0
+    }
+  ] 
 }
